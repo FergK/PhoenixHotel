777cb0658d84f6e8fad92e41fd40fd872f2bbc48
oromeo10 <rocampo3@student.gsu.edu>
2016-10-23 00:15:43 -0400

    2nd draft eventRoom class

---
+++ src/src/prms/eventRoom.java
@@ -0,0 +1,68 @@
+
+public class eventRoom {
+	private String roomNumber;
+	private double price;
+	private int party;
+	private int hasStage;
+	private int hasAudioVisual;
+	
+	public eventRoom(String room, double price, int party, int hasStage, int hasAudioVisual){
+		this.roomNumber= room;
+		this.price= price;
+		this.party= party;
+		this.hasStage= hasStage;
+		this.hasAudioVisual= hasAudioVisual;
+	}
+
+	public String getRoomNumber() {
+		return roomNumber;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public int getParty() {
+		return party;
+	}
+
+	public boolean getHasStage() {
+		if(this.hasStage==1){
+			return true;
+		}
+		return false;
+	}
+
+	public boolean getHasAudioVisual() {
+		if(this.hasAudioVisual==1){
+			return true;
+		}
+		return false;
+	}
+
+	public void setRoomNumber(String roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public void setParty(int party) {
+		this.party = party;
+	}
+
+	public void setHasStage(int hasStage) {
+		this.hasStage = hasStage;
+	}
+
+	public void setHasAudioVisual(int hasAudioVisual) {
+		this.hasAudioVisual = hasAudioVisual;
+	}
+	
+	public String toString(){
+		return "Room Number: "+this.getRoomNumber()+"\nPrice: $"+this.getPrice()+"\nParty: "+this.getParty()+"Has Stage: "+this.getHasStage()+"Has Audio/Visual Equipment: "+this.getHasAudioVisual();
+	}
+
+	
+}
