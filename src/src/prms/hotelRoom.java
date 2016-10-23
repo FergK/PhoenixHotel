e6063e283ac1ca956bc86cf28aa180f80e5cc801
oromeo10 <rocampo3@student.gsu.edu>
2016-10-22 23:22:29 -0400

    Uploaded 2nd draft of HotelRoom class

---
+++ src/src/prms/hotelRoom.java
@@ -0,0 +1,88 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class hotelRoom {
+	private double price;
+	private int beds;
+	private int hasPets;
+	private int disabilityAccessible;
+	private int allowsSmoking;
+	private int startDate;		//assumed to be unix timestamp in database//
+	private int endDate;		//assumed to be unix timestamp in database//
+	
+	public hotelRoom(int price, int beds, int hasPets, int disableAccessible, int startDate, int endDate ){
+		this.price=price;
+		this.beds=beds;
+		this.hasPets=hasPets;
+		this.disabilityAccessible= disableAccessible;
+		this.startDate= startDate;
+		this.endDate= endDate;
+		
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public int getBeds() {
+		return beds;
+	}
+
+	public boolean hasPets(){
+		if (hasPets==1){
+			return true;
+		}
+		return false;
+	}
+
+	public boolean getDisabilityAccessible() {
+		if(disabilityAccessible==1){
+			return true;
+		}
+		return false;
+	}
+
+	public boolean allowsSmoking(){
+		if(allowsSmoking==1){
+			return true;
+		}
+		return false;
+	}
+	public int getStartDate() {
+		return startDate;
+	}
+
+	public int getEndDate() {
+		return endDate;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public void setBeds(int beds) {
+		this.beds = beds;
+	}
+
+	public void setHasPets(int hasPets) {
+		this.hasPets = hasPets;
+	}
+
+	public void setDisabilityAccessible(int disabilityAccessible) {
+		this.disabilityAccessible = disabilityAccessible;
+	}
+
+	public void setStartDate(int startDate) {
+		this.startDate = startDate;
+	}
+
+	public void setEndDate(int numberOfDays) {
+		this.endDate = startDate + numberOfDays;
+	}
+	
+	public String toString(){
+		return "Room price: $"+this.getPrice()+"\n# of beds: "+this.beds+"\nDisability Accessible: "+this.getDisabilityAccessible()+"\nHas Pets: "+this.hasPets()+"\nAllows Smoking: "+this.allowsSmoking()+"\nStart Date: "+this.startDate+"End Date: "+this.endDate;
+		
+	}
+	
+}
