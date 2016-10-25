4630350ef2e019714dc07257525e283e0377d19b
Fergus Kelley <i@FergK.com>
2016-10-25 14:10:43 -0400

    added roomNUmber field to reservation

--- src/src/prms/HotelReservation.java
+++ src/src/prms/HotelReservation.java
@@ -9,18 +9,24 @@ import java.time.LocalDate;
 
 public class HotelReservation {
 
+    private String roomNumber;
     private int adults;
     private int children;
     private LocalDate startDate;
     private LocalDate endDate;
     private Invoice bill;
 
-    public HotelReservation(int adults, int children, LocalDate startDate, LocalDate endDate, Invoice bill) {
+    public HotelReservation(String roomNumber, int adults, int children, LocalDate startDate, LocalDate endDate, Invoice bill) {
+        this.roomNumber = roomNumber;
         this.adults = adults;
         this.children = children;
         this.startDate = startDate;
         this.endDate = endDate;
         this.bill = bill;
+    }
+
+    public String getRoomNumber() {
+        return roomNumber;
     }
 
     public int getAdults() {
