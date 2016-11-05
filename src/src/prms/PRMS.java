0c271678da4a8662e0501194b1f865933f4b11d3
ItsAllAndrew <atruong8@yahoo.com>
2016-11-05 14:56:33 -0400

    Update PRMS.java

--- src/src/prms/PRMS.java
+++ src/src/prms/PRMS.java
@@ -121,6 +121,78 @@ public class PRMS extends Application {
                     + ");";
             stmt.execute(sql);
 
+            sql = "CREATE TABLE IF NOT EXISTS inventoryitem (\n"
+            + " name    TEXT    PRIMARY KEY   NOT NULL,\n"
+            + " roomnumber         TEXT    NOT NULL,\n"
+            + " quanitity          INT     NOT NULL,\n"
+            + " expectedQuantity      INT    NOT NULL,\n"
+            + " isConsumable      NUMERIC    NOT NULL,\n"
+            + ");";
+        stmt.execute(sql);
+            
+            sql = "CREATE TABLE IF NOT EXISTS invoice (\n"
+                + " UID    INT    PRIMARY KEY   NOT NULL,\n"
+                + " customerName         TEXT    NOT NULL,\n"
+                + " CCNum          TEXT     NOT NULL,\n"
+                + " CCExp    INT    NOT NULL,\n"
+                + ");";
+            stmt.execute(sql);
+            
+            sql = "CREATE TABLE IF NOT EXISTS restaurant (\n"
+                    + " numofTable    TEXT    PRIMARY KEY   NOT NULL,\n"
+                    + ");";
+             stmt.execute(sql); 
+            
+            sql = "CREATE TABLE IF NOT EXISTS billableitems (\n"
+                        + " billableName    TEXT    PRIMARY KEY   NOT NULL,\n"
+                        + " price         REAL    NOT NULL,\n"
+                        + " date          INT     NOT NULL,\n"
+                        + " time      INT    NOT NULL,\n"
+                        + ");";
+             stmt.execute(sql);
+            
+            sql = "CREATE TABLE IF NOT EXISTS order (\n"
+                            + " InvoiceNumber    TEXT    PRIMARY KEY   NOT NULL,\n"
+                            + " orderDate         INT    NOT NULL,\n"
+                            + " orderStatus          INT     NOT NULL,\n"
+                            + " billables      TEXT    NOT NULL,\n"
+                            + ");";
+             stmt.execute(sql);
+            
+             sql = "CREATE TABLE IF NOT EXISTS roomserviceorder (\n"
+                                + " roomNumber   INT    PRIMARY KEY   NOT NULL,\n"
+                                + ");";
+              stmt.execute(sql);
+            
+             sql = "CREATE TABLE IF NOT EXISTS tableserviceorder (\n"
+                                    + " tableNumber    INT    PRIMARY KEY   NOT NULL,\n"
+                                    + ");";
+               stmt.execute(sql);
+            
+            sql = "CREATE TABLE IF NOT EXISTS cateredmealorder (\n"
+                                        + " roomname    TEXT    PRIMARY KEY   NOT NULL,\n"
+                                        + ");";
+             stmt.execute(sql);
+            
+            sql = "CREATE TABLE IF NOT EXISTS maintenanceorder (\n"
+                                            + " roomname    TEXT    PRIMARY KEY   NOT NULL,\n"
+                                            + " description         TEXT    NOT NULL,\n"
+                                            + ");";
+              stmt.execute(sql);
+            
+            sql = "CREATE TABLE IF NOT EXISTS restaurantmenuitem (\n"
+                                                + " itemname    TEXT    PRIMARY KEY   NOT NULL,\n"
+                                                + " price         REAL    NOT NULL,\n"
+                                                + " description          TEXT     NOT NULL,\n"
+                                                + ");";
+              stmt.execute(sql);
+            
+            sql = "CREATE TABLE IF NOT EXISTS cateredmealitem (\n"
+                                                    + " mealname    TEXT    PRIMARY KEY   NOT NULL,\n"
+                                                    + " priceperseat         REAL    NOT NULL,\n"
+                                                    + " caterdescription          TEXT     NOT NULL,\n"
+                                                    + ");";
+               stmt.execute(sql);
             
             rs.close();
             stmt.close();
